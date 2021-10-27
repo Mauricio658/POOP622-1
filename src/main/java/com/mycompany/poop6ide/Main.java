c021d6357dd84d4d31d98cfdeb3d13b4bba7c774
Angel Mauricio Jimenez Cervantes <79766430+Mauricio658@users.noreply.github.com>
2021-10-27 18:45:43 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/poop6ide/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poop6ide;
+
+/**
+ *
+ * @author jimen
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Clse enpaquetada desde el IDE");
+        System.out.println("La suma es: "+suma(2,2));
+        System.out.println("Nueva linea");
+    }
+    /**
+     * Retorna una suma de dos enteros a y b
+     * @param a Numero Entero Primero
+     * @param b Numero Entero Segundo
+     * @return la suma de a y b
+     */
+    public static int suma(int a,int b){
+        return a+b;
+    }
+}
